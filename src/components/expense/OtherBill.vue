38df951d9919e37cc119f3e4979ff78dff6494b2
rafi0x <rafiulawal.r@gmail.com>
2023-05-06 01:16:16 +0600

    image upload  add

--- src/components/expense/OtherBill.vue
+++ src/components/expense/OtherBill.vue
@@ -2,19 +2,20 @@
   <div>
     <q-card flat class="my-card">
       <q-card-section class="grid grid-cols-2 gap-5">
-        <q-input outlined v-model="formData.reason" placeholder="Reason" :dense="true" />
-        <q-input outlined v-model="formData.amount" placeholder="Total Amount" bg-color="white" :dense="true" />
-        <q-input outlined v-model="formData.notes" placeholder="Notes" bg-color="white" :dense="true" />
+        <q-input outlined v-model="data.reason" placeholder="Reason" :dense="true" />
+        <q-input outlined v-model="data.amount" placeholder="Total Amount" bg-color="white" :dense="true" />
+        <q-input outlined v-model="data.notes" placeholder="Notes" bg-color="white" :dense="true" />
         <div>
-          <q-uploader url="http://localhost:4444/upload" label="Individual upload" color="green" multiple
+          <q-uploader label="Individual upload" color="green" multiple @added="onAdded" @removed="onRemoved"
             style="max-width: 300px" />
         </div>
 
 
         <div class="flex items-end justify-end">
 
-          <q-btn v-if="formData.amount" label="Update" color="green" class="px-10 py-2.5" />
-          <q-btn v-if="!formData.amount" label="Save" color="green" class="px-10 py-2.5" @click="registerUser" />
+          <q-btn v-if="$props.expenseData?.amount" label="Update" color="green" class="px-10 py-2.5" />
+          <q-btn v-if="!$props.expenseData?.amount" label="Save" color="green" class="px-10 py-2.5"
+            @click="registerUser" />
 
         </div>
       </q-card-section>
@@ -23,7 +24,7 @@
 </template>
 
 <script>
-import { defineComponent, reactive, onMounted } from 'vue';
+import { defineComponent } from 'vue';
 // import SearchAddCompo from '../SearchAddCompo.vue';
 export default defineComponent({
   components: {
@@ -32,29 +33,57 @@ export default defineComponent({
   props: {
     expenseData: Object,
   },
-  setup(props) {
-    const formData = reactive({
-      request_type: 'OTHERS',
-      reason: null,
-      amount: null,
-      notes: null,
-    });
-
-    onMounted(() => {
-      console.log(props.expenseData.amount);
-      if (props.expenseData) {
-        formData.reason = props.expenseData.reason;
-        formData.amount = props.expenseData.amount;
-        formData.notes = props.expenseData.notes;
-      }
-    });
+  data() {
     return {
-      formData,
+      maxFileSize: 3 * 1024 * 1024, // 3MB
+      images: [],
+      data: {
+        request_type: 'OTHERS',
+        reason: null,
+        amount: null,
+        notes: null,
+      }
+    };
+  },
+  mounted() {
+    if (this.$props.expenseData) {
+      this.data.reason = this.$props.expenseData.reason;
+      this.data.amount = this.$props.expenseData.amount;
+      this.data.notes = this.$props.expenseData.notes;
     }
   },
   methods: {
     registerUser() {
-      this.$emit('addExpense', this.formData);
+      const formData = new FormData();
+
+      Object.keys(this.data).forEach((key) => {
+        formData.append(key, this.data[ key ]);
+      });
+
+      this.images.forEach((image) => {
+        formData.append('images', image);
+      });
+
+      this.$emit('addExpense', formData);
+    },
+    onAdded(files) {
+      files.forEach((file) => {
+        if (file.size > this.maxFileSize) {
+          this.$q.notify({
+            color: 'negative',
+            message: 'File size exceeded',
+            icon: 'report_problem',
+          });
+          return;
+        }
+        this.images.push(file);
+      });
+    },
+    onRemoved(files) {
+      this.images = this.images.filter((image) => {
+        return !files.includes(image);
+      });
+      console.log("🚀 ~ file: OtherBill.vue:81 ~ this.images=this.images.filter ~ this.images:", this.images)
     }
   }
 })
