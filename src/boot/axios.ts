0c1df4b5e5dae89bc482872bb254b87a792b9f32
rafi0x <rafiulawal.r@gmail.com>
2023-05-06 17:48:03 +0600

    api updates

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -10,7 +10,7 @@ declare module '@vue/runtime-core' {
 const $api = axios.create({
   // baseURL: 'http://localhost:8000/api',
   // baseURL: 'http://192.168.0.195:5000/api/v1',
-  baseURL: 'http://brain.encore.com.bd/api',
+  baseURL: 'https://brain.encore.com.bd/api',
 });
 
 export default boot(({ app }) => {
