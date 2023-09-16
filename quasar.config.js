05e634d81135b5c95c32dc8d615f220a0d9d492d
Rafi <rafiulawal.r@gmail.com>
2023-09-17 00:18:23 +0600

    t

--- quasar.config.js
+++ quasar.config.js
@@ -191,9 +191,6 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
     bex: {
       contentScripts: ['my-content-script'],
-
-      // extendBexScriptsConf (esbuildConf) {}
-      // extendBexManifestJson (json) {}
     },
   };
 });
